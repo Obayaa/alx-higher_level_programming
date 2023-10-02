4ce30d705e1c3f586700fbfe65e84eb81dc66db1
Dora Appiah <111066711+Obayaa@users.noreply.github.com>
2023-10-02 21:34:38 +0000

    Update

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a request and only show the status code
-curl -s -o /dev/null -I --w "%{http_code}" "$1"
+# Sends a GET request and displays the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
