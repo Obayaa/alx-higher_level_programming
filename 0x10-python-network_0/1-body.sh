deb22b8be49b27bfabd2032b82b5c7ca92ed836f
Dora Appiah <111066711+Obayaa@users.noreply.github.com>
2023-10-02 21:30:30 +0000

    Update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# sends GET request to the URL and displays the body of the response
+curl -sfL "$1" -X GET
